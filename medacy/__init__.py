4be5cf1ace6856055c526b46d3e72f4e4806bbb0
Andriy Mulyar <andriy.mulyar@gmail.com>
2018-12-10 03:53:58 -0500

    Final touches for sub-version bump to v0.0.3

--- medacy/__init__.py
+++ medacy/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '0.0.2'
-__authors__ = "Andriy Mulyar, Bobby Best, Steele Farnsworth, Yadunandan Pillai, Corey Sutphin, Bridget McInnes"
+__version__ = '0.0.3'
+__authors__ = "Andriy Mulyar, Corey Sutphin, Bobby Best, Steele Farnsworth, Bridget McInnes"
