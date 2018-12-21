bc4791a27642367af3651bd3330c72369aa5726c
Andriy Mulyar <aymulyar@vcu.edu>
2018-12-20 20:35:35 -0500

    Modified load_END code, re-wrote unit tests to utilize new dataset present in package

--- medacy/data/load_dataset.py
+++ medacy/data/load_dataset.py
@@ -7,16 +7,17 @@ def load_END():
     """
     Loads the Engineered Nanomedicine Database (END) for testing purposes. This dataset is a collection of annotated
     FDA nanomedicine drug labels containing 28 unique entities.
+    Dataset citation: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5644562/
     :return: a tuple containing an generator of (raw_text, ann_file) tuples and an array of target entities
     """
-    package_name = 'medacy.tests.data'
+    package_name = 'medacy.data'
     resource_directory = 'end'
     contents = resource_listdir(package_name, resource_directory)
-    entities = resource_string('medacy.tests.data', resource_directory+'/END.types').decode('utf-8').split("\n")
+    entities = resource_string(package_name, resource_directory+'/END.types').decode('utf-8').split("\n")
     files = [file[:-4] for file in contents if file.endswith('.ann')]
     files = ((file_name,
-              resource_string('medacy.tests.data', resource_directory+"/%s.txt" % file_name).decode('utf-8'),
-              resource_string('medacy.tests.data', resource_directory + "/%s.ann" % file_name).decode('utf-8')
+              resource_string(package_name, resource_directory+"/%s.txt" % file_name).decode('utf-8'),
+              resource_string(package_name, resource_directory + "/%s.ann" % file_name).decode('utf-8')
               )
              for file_name in files)
     return files, entities
