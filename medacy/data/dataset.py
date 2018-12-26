42edd8cfe418ea1d74b567f09c5ba828dfaed202
Andriy Mulyar <aymulyar@vcu.edu>
2018-12-26 01:13:25 -0500

    Added Dataset class that builds on DataLoader functionality while allowing for the import of outside medaCy compatible datasets.

--- medacy/data/dataset.py
+++ medacy/data/dataset.py
@@ -19,32 +19,45 @@ Dataset examples. Existing Datasets can be imported by installing the relevant p
 wrap them.
 """
 from medacy.tools import DataFile
-import os, logging
+from joblib import Parallel, delayed
+import os, logging, multiprocessing, math, json, importlib
+
 
 class Dataset:
     """
     A facilitation class for data management.
     """
 
-    def __init__(self, data_directory, raw_text_file_extension="txt", annotation_file_extension="ann",  limit=None):
+    def __init__(self, data_directory,
+                 raw_text_file_extension="txt",
+                 annotation_file_extension="ann",
+                 metamapped_files_directory = None,
+                 limit=None):
         """
         Manages directory of training data along with other medaCy generated files.
         :param data_directory: Directory containing data for training or prediction.
         :param raw_text_file_extension: The file extension of raw text files in the data_directory (default: *.txt*)
         :param annotation_file_extension: The file extension of annotation files in the data_directory (default: *.ann*)
+        :param metamapped_files_directory: Location to store metamapped files (default: a sub-directory named *metamapped*)
         :param limit: A limit to the number of files to process. Must be between 1 and number of raw text files in data_directory
         """
         self.data_directory = data_directory
         self.raw_text_file_extension = raw_text_file_extension
         self.all_data_files = []
-        all_files_in_directory = os.listdir(data_directory)
+
+        if metamapped_files_directory is not None:
+            self.metamapped_files_directory = metamapped_files_directory
+        else:
+            self.metamapped_files_directory = os.path.join(self.data_directory, 'metamapped')
+
+        all_files_in_directory = os.listdir(self.data_directory)
 
         # start by filtering all raw_text files, both training and prediction directories will have these
         raw_text_files = sorted([file for file in all_files_in_directory if file.endswith(raw_text_file_extension)])
 
-        if raw_text_files is not None:
+        if raw_text_files is None:
             raise ValueError("No raw text files exist in directory: %s" % self.data_directory)
-        if limit is not None and limit < 1 or limit > len(raw_text_files):
+        if limit is not None and (limit < 1 or limit > len(raw_text_files)):
             raise ValueError("Parameter 'limit' must be between 1 and number of raw text files in data_directory")
 
         if limit is not None and 0 < limit and limit <= len(raw_text_files):
@@ -69,32 +82,149 @@ class Dataset:
                 annotation_path = None
             self.all_data_files.append(DataFile(file_name, raw_text_path, annotation_path))
 
+        #If directory is already metamapped, use it.
+        if self.is_metamapped():
+            for data_file in self.all_data_files:
+
+                data_file.metamapped_path = os.path.join(self.metamapped_files_directory,
+                                                         data_file.raw_path.split(os.path.sep)[-1]
+                                                         .replace(".%s" % self.raw_text_file_extension, ".metamapped"))
+
+
+    def get_files(self):
+        """
+        Retrieves an list containing all the files registered by a Dataset.
+        :return: a list of DataFile objects.
+        """
+        return self.all_data_files
+
+    def metamap(self, metamap, n_jobs=multiprocessing.cpu_count() - 1, retry_possible_corruptions=True):
+        """
+        Metamaps the files registered by a Dataset. Attempts to Metamap utilizing a max prune depth of 30, but on
+        failure retries with lower max prune depth. A lower prune depth roughly equates to decreased MetaMap performance.
+        More information can be found in the MetaMap documentation.
+        :param metamap: an instance of MetaMap.
+        :param n_jobs: the number of processes to spawn when metamapping. Defaults to one less core than available on your machine.
+        :param retry_possible_corruptions: Re-Metamap's files that are detected as being possibly corrupt. Set to False for more control over what gets Metamapped or if you are having bugs with Metamapping. (default: True)
+        :return: Inside *metamapped_files_directory* or by default inside a sub-directory of your *data_directory* named *metamapped* we have that for each raw text file, *file_name*, an auxiliary metamapped version is created and stored.
+        """
+        self.metamap = metamap
+
+        if self.is_metamapped():
+            return True
+
+        #make metamap directory if it doesn't exist.
+        if not os.path.isdir(self.metamapped_files_directory):
+            os.makedirs(self.metamapped_files_directory)
+
+        #A file that is below 200 bytes is likely corrupted output from MetaMap, these should be retried.
+        if retry_possible_corruptions:
+            #Do not metamap files that are already metamapped and above 200 bytes in size
+            already_metamapped = [file[:file.find('.')] for file in os.listdir(self.metamapped_files_directory)
+                                  if os.path.getsize(os.path.join(self.metamapped_files_directory, file)) > 200]
+        else:
+            #Do not metamap files that are already metamapped
+            already_metamapped = [file[:file.find('.')] for file in os.listdir(self.metamapped_files_directory)]
+
+        files_to_metamap = [data_file.raw_path for data_file in self.all_data_files if not data_file.file_name in already_metamapped]
+
+        logging.info("Number of files to MetaMap: %i" % len(files_to_metamap))
+
+        Parallel(n_jobs=n_jobs)(delayed(self._parallel_metamap)(files_to_metamap, i) for i in range(len(files_to_metamap)))
+
+        if self.is_metamapped():
+            for data_file in self.all_data_files:
+                data_file.metamapped_path = os.path.join(self.metamapped_files_directory,
+                                                         data_file.raw_path.split(os.path.sep)[-1]
+                                                         .replace(".%s" % self.raw_text_file_extension, ".metamapped"))
+
+
+    def _parallel_metamap(self, files, i):
+        """
+        Facilitates Metamapping in parallel by forking off processes to Metamap each file individually.
+        :param files: an array of file paths to the file to map
+        :param i: index in the array used to determine the file that the called process will be responsible for mapping
+        :return: metamapped_files_directory now contains metamapped versions of the dataset files
+        """
+        file = files[i].split(os.path.sep)[-1]
+        file_path = files[i]
+        logging.info("Attempting to Metamap: %s", file_path)
+        mapped_file_location = os.path.join(self.metamapped_files_directory, file.replace(self.raw_text_file_extension, "metamapped"))
+
+        with open(mapped_file_location, 'w') as mapped_file:
+            max_prune_depth = 30  # this is the maximum prune depth metamap utilizes when concept mapping
+
+            metamap_dict = None
+            # while current prune depth causes out of memory on document
+            while metamap_dict is None or metamap_dict['metamap'] is None:
+                if max_prune_depth <= 0:
+                    logging.critical("Failed to to metamap after multiple attempts: %s", file_path)
+                    return
+                try:
+                    metamap_dict = self.metamap.map_file(file_path, max_prune_depth=max_prune_depth) #attempt to metamap
+                    if metamap_dict['metamap'] is not None: #if successful
+                        break
+                    max_prune_depth = int(math.e ** (math.log(max_prune_depth) - .5)) #decrease prune depth by an order of magnitude
+                except BaseException as e:
+                    metamap_dict = None
+                    max_prune_depth = int(math.e ** (math.log(max_prune_depth) - .5)) #decrease prune depth by an order of magnitude
+                    logging.warning("Error Metamapping: %s with exception %s", file_path, str(e))
+
+            mapped_file.write(json.dumps(metamap_dict))
+            logging.info("Successfully Metamapped: %s", file_path)
+            logging.info("Successfully Metamapped: %s" % file_path)
 
     def is_metamapped(self):
         """
         Verifies if all files in the Dataset are metamapped.
-        :return: True if all data files are metamapped, False otherwise
+        :return: True if all data files are metamapped, False otherwise.
         """
 
-        if not os.path.isdir(self.data_directory+"/metamapped/"):
+        if not os.path.isdir(self.metamapped_files_directory):
             return False
 
         for file in self.all_data_files:
-            potential_file_path = os.path.join(self.data_directory + "/metamapped",
-                                               file.replace(".%s" % self.raw_text_file_extension, ".metamapped"))
+            potential_file_path = os.path.join(self.metamapped_files_directory,
+                                               file.file_name.replace(".%s" % self.raw_text_file_extension, ".metamapped"))
             if not os.path.isfile(potential_file_path):
                 return False
 
-            #File could exist, but metamapping it could have failed.
-            #If the file is less than 200 bytes, log a warning.
+            # Metamapped file could exist, but metamapping it could have failed.
+            # If the file is less than 200 bytes, log a warning.
             file_size_in_bytes = os.path.getsize(potential_file_path)
             if file_size_in_bytes < 200:
                 logging.warning("Metamapped version of %s is only %i bytes. Metamapping could have failed: %s" %
                                 (file.file_name, file_size_in_bytes, potential_file_path))
-        
-        return True
-
 
         return True
-        mapped_file_location =
-        return os.path.isdir(self.data_directory+"/metamapped/")
+
+    def is_training(self):
+        """
+        Whether this Dataset can be used for training.
+        :return: True if training dataset, false otherwise. A training dataset is a collection raw text and corresponding annotation files while a prediction dataset contains solely raw text files.
+        """
+        return self.is_training_directory
+
+
+
+    @staticmethod
+    def load_external(package_name):
+        """
+        Loads an external medaCy compatible dataset. Requires the dataset's associated package to be installed.
+        Alternatively, you can import the package directly and call it's .load() method.
+        :param package_name: the package name of the dataset
+        :return: an instance of Dataset that wraps the re
+        """
+        spec = importlib.util.find_spec(package_name)
+        if spec is None:
+            raise ValueError("Package not installed: %s" % package_name)
+        return importlib.import_module(package_name).load()
+
+
+
+
+
+
+
+
+
