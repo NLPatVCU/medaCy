79c0f34c52b643112080a68e6fcb42f480fd1344
Andriy Mulyar <aymulyar@vcu.edu>
2018-12-27 01:12:11 -0500

    Downgraded msgpack

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         'joblib>=0.12.5',
         'tabulate>=0.8.2',
         'pathos>=0.2.2.1',
+        'msgpack>=0.3.0,<0.6',
         'en_core_web_sm'
     ],
     tests_require=["pytest", "medacy_dataset_end"],
