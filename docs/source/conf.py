d03f83119b8674980d7a8bdcc572a33d4f156e80
Andriy Mulyar <andriy.mulyar@gmail.com>
2019-01-02 12:49:55 -0500

    Updated version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Andriy Mulyar, Corey Sutphin, Bobby Best, Steele Farnsworth, Bridget M
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '0.6'
 
 
 # -- General configuration ---------------------------------------------------
