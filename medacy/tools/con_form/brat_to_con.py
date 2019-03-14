13392a564427f89d19e4f31e6a85ae07e6a982f3
swfarnsworth <swfarnsworth@gmail.com>
2019-03-13 20:19:20 -0400

    Improved accuracy for brat_to_con

--- medacy/tools/con_form/brat_to_con.py
+++ medacy/tools/con_form/brat_to_con.py
@@ -3,31 +3,40 @@ Converts data from brat to con. Enter input and output directories as command li
 Each '.ann' file must have a '.txt' file in the same directory with the same name, minus the extension.
 Use '-c' (without quotes) as an optional final command-line argument to copy the text files used
 in the conversion process to the output directory.
+
 Also possible to import 'convert_brat_to_con()' directly and pass the paths to the ann and txt files
 for individual conversion.
 
 :author: Steele W. Farnsworth
-:date: 16 February, 2019
+:date: 13 March, 2019
 """
 
 from sys import argv
 from re import split, fullmatch, DOTALL, findall
+from medacy.tools.converters.conversion_tools.line import Line
+import re
 import os
 import shutil
 import logging
+import tabulate
 
 
 # A regex pattern for consecutive whitespace other than a new line character
-whitespace_pattern = "( +|\t+)+"
+whitespace_pattern = re.compile("( +|\t+)+")
+# Regex pattern for BRAT T annotations
+brat_pattern_T = r"T\d+\t\S+ \d+ \d+\t.+"
+
+# Used for stats at the end
+num_lines = 0
+num_skipped_regex = 0
 
 
 def is_valid_brat(item: str):
     """Returns a boolean value for whether or not a given line is in the BRAT format."""
     # Define the regex pattern for BRAT.
     # Note that this pattern allows for three to six spaces to count as a tab
-    brat_pattern = r"[TREAMN]\d+(\t| {3,6})\S+ \d+ \d+(\t| {3,6}).+"
     if not isinstance(item, str): return False
-    if fullmatch(brat_pattern, item, DOTALL): return True
+    if fullmatch(brat_pattern_T, item, DOTALL): return True
     else: return False
 
 
@@ -41,7 +50,7 @@ def line_to_dict(item):
     split1 = split("\t", item)
     split2 = split(" ", split1[1])
     split3 = [split1[0]] + split2 + [split1[2]]
-    s = [i.rstrip() for i in split3]  # remove whitespace
+    s = [i.strip() for i in split3]  # remove whitespace
     return {"id_type": s[0][0], "id_num": int(s[0][1:]), "data_type": s[1], "start_ind": int(s[2]),
             "end_ind": int(s[3]), "data_item": s[4]}
 
@@ -54,12 +63,6 @@ def switch_extension(name, ext):
     return os.path.splitext(name)[0] + ext
 
 
-def get_line_index(text_, line_):
-    """Returns the index of the start of a given line. Assumes that the line_
-    argument is long enough that (and thus so specific that) it only occurs once."""
-    return text_.index(line_)
-
-
 def find_line_num(text_, start):
     """
     :param text_: The text of the file, ex. f.read()
@@ -69,19 +72,18 @@ def find_line_num(text_, start):
     return text_[:int(start)].count("\n")
 
 
-def get_word_num(text_, line_index, entity_index):
+def get_word_num(line_obj: Line, entity_index):
     """
-    Returns the word number starting at zero that a given BRAT entity start index is within its line.
-    In the previous line, "Returns" is word 0 and "starting" is word 4. Words are counted by the number of consecutive
-    white spaces.
-    :param text_: The text of the document that the word occurs in.
-    :param line_index: The index of the first char of the line the word occurs in.
-    :param entity_index: The index of the first char of the word relative to the start of the document.
-    :return: The word number (see above explanation for what a word number is) of the given index within its line.
+    Returns the word number relative to the start of the line, with counting starting at 0,
+    of the first word of the entity.
+    :param line_obj: The Line that the entity occurs in.
+    :param entity_index: The absolute index of the entity, given by the annotation.
+    :return: The word index of the entity.
     """
-    substring_before_entity = text_[line_index:entity_index]
+    index_within_line = entity_index - line_obj.index
+    substring_before_entity = line_obj.text[:index_within_line]
     matched_spaces = findall(whitespace_pattern, substring_before_entity)
-    return matched_spaces.__len__()
+    return len(matched_spaces)
 
 
 def convert_brat_to_con(brat_file_path, text_file_path=None):
@@ -94,6 +96,8 @@ def convert_brat_to_con(brat_file_path, text_file_path=None):
     :return: A string (not a file) of the con equivalent of the brat file.
     """
 
+    global num_lines, num_skipped_regex
+
     # By default, find txt file with equivalent name
     if text_file_path is None:
         text_file_path = switch_extension(brat_file_path, ".txt")
@@ -102,12 +106,12 @@ def convert_brat_to_con(brat_file_path, text_file_path=None):
                                     " directory")
         with open(text_file_path, 'r') as text_file:
             text = text_file.read()
-            text_lines = text.split('\n')
+            text_lines = Line.init_lines(text)
     # Otherwise open the file with the path passed to the function
     elif os.path.isfile(text_file_path):
         with open(text_file_path, 'r') as text_file:
             text = text_file.read()
-            text_lines = text.split('\n')
+            text_lines = Line.init_lines(text)
     else: raise FileNotFoundError("No text file path was provided or the file was not found."
                                   " Note that direct string input of the source text is not supported.")
 
@@ -129,24 +133,24 @@ def convert_brat_to_con(brat_file_path, text_file_path=None):
             continue
         elif not is_valid_brat(line):
             logging.warning("Incorrectly formatted line in %s was skipped: \"%s\"." % (brat_file_path, line))
+            num_skipped_regex += 1
             continue
 
         d = line_to_dict(line)
 
         start_line_num = find_line_num(text, d["start_ind"])
-        start_text_line = text_lines[start_line_num]
-        start_line_index = get_line_index(text, start_text_line)
-        start_word_num = get_word_num(text, start_line_index, d["start_ind"])
+        start_source_line = text_lines[start_line_num]
+        start_word_num = get_word_num(start_source_line, d["start_ind"])
         start_str = str(start_line_num + 1) + ':' + str(start_word_num)
 
         end_line_num = find_line_num(text, d["end_ind"])
-        end_text_line = text_lines[end_line_num]
-        end_line_index = get_line_index(text, end_text_line)
-        end_word_num = get_word_num(text, end_line_index, d["end_ind"])
+        end_word_num = start_word_num + len(re.findall(whitespace_pattern, d["data_item"]))
         end_str = str(end_line_num + 1) + ':' + str(end_word_num)
 
         con_line = "c=\"%s\" %s %s||t=\"%s\"\n" % (d["data_item"], start_str, end_str, d['data_type'])
         output_lines += con_line
+
+        num_lines += 1
 
     return output_lines
 
@@ -155,13 +159,11 @@ if __name__ == '__main__':
 
     # Get the input and output directories from the command line.
 
-    if not argv.__len__() >= 3:
+    if len(argv) < 3:
         # Command-line arguments must be provided for the input and output directories.
-        # Else, prints instructions and aborts the program.
-        print("Please run the program again, entering the input and output directories as command-line arguments"
-              " in that order. Optionally, enter '-c' as a final command line argument if you want to copy"
-              " the text files used in the conversion over to the output directory.")
-        exit()
+        raise IOError("Please run the program again, entering the input and output directories as command-line"
+                      " arguments in that order. Optionally, enter '-c' as a final command line argument if you want"
+                      " to copy the text files used in the conversion over to the output directory.")
 
     try:
         input_dir_name = argv[1]
@@ -188,7 +190,6 @@ if __name__ == '__main__':
         raise FileNotFoundError("There were no ann files in the input directory with a corresponding text file. "
                                 "Please ensure that the input directory contains ann files and that each file has "
                                 "a corresponding txt file (see help for this program).")
-        exit()
 
     # Create the log
     log_path = os.path.join(output_dir_name, "conversion.log")
@@ -203,8 +204,22 @@ if __name__ == '__main__':
 
     # Paste all the text files used in the conversion process to the output directory
     # if there's a fourth command line argument and that argument is -c
-    if argv.__len__() == 4 and argv[3] == "-c":
+    if len(argv) >= 4 and argv[3] == "-c":
         text_files_with_match = [f for f in text_files if switch_extension(f, ".ann") in ann_files]
         for f in text_files_with_match:
             full_name = os.path.join(input_dir_name, f)
             shutil.copy(full_name, output_dir_name)
+
+    # Compile and print stats to log
+    stat_headers = ["Total lines", "Total converted",
+                    "Skipped did not match regex", "Percent converted"]
+
+    stat_data = [
+        num_lines,
+        num_lines - num_skipped_regex,
+        num_skipped_regex,
+        (num_lines - num_skipped_regex) / num_lines
+    ]
+
+    conversion_stats = tabulate.tabulate(headers=stat_headers, tabular_data=[stat_data])
+    logging.warning("\n" + conversion_stats)
