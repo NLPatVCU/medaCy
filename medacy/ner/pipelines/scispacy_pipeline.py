370bad2321dde1ed3dadab0c2fb1ff7698ee07a0
swfarnsworth <swfarnsworth@gmail.com>
2019-06-20 16:19:56 -0400

    Initial commit

---
+++ medacy/ner/pipelines/scispacy_pipeline.py
@@ -0,0 +1,52 @@
+import spacy, sklearn_crfsuite
+from .base import BasePipeline
+from medacy.pipeline_components import ClinicalTokenizer
+from medacy.pipeline_components.feature_extraction.discrete_feature_extractor import FeatureExtractor
+from medacy.pipeline_components import GoldAnnotatorComponent, MetaMapComponent, MetaMap
+
+
+class ScispacyPipeline(BasePipeline):
+    """
+    A pipeline for named entity recognition using ScispaCy, see https://allenai.github.io/scispacy/
+    """
+
+    def __init__(self, metamap=None, entities=[]):
+        """
+        Create a pipeline with the name 'clinical_pipeline' utilizing
+        by default spaCy's small english model.
+
+        :param metamap: an instance of MetaMap if metamap should be used, defaults to None.
+        """
+        super().__init__("scispacy_pipeline",
+                         spacy_pipeline=spacy.load("en_core_sci_md"),
+                         description=self.__doc__,
+                         creators="Steele W. Farnsworth",  # append if multiple creators
+                         organization="NLP@VCU"
+                         )
+
+        self.entities = entities
+
+        self.spacy_pipeline.tokenizer = self.get_tokenizer() #set tokenizer
+
+        self.add_component(GoldAnnotatorComponent, entities) #add overlay for GoldAnnotation
+
+        if metamap is not None and isinstance(metamap, MetaMap):
+            self.add_component(MetaMapComponent, metamap)
+
+    def get_learner(self):
+        return ("CRF_l2sgd",
+                sklearn_crfsuite.CRF(
+                    algorithm='l2sgd',
+                    c2=0.1,
+                    max_iterations=100,
+                    all_possible_transitions=True
+                )
+            )
+
+    def get_tokenizer(self):
+        tokenizer = ClinicalTokenizer(self.spacy_pipeline)
+        return tokenizer.tokenizer
+
+    def get_feature_extractor(self):
+        extractor = FeatureExtractor(window_size=3, spacy_features=['pos_', 'shape_', 'prefix_', 'suffix_', 'text'])
+        return extractor
