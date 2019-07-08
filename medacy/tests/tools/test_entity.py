f7e61af490c6147dfaa81ade4e7cd02c1c46aa89
swfarnsworth <swfarnsworth@gmail.com>
2019-07-08 13:14:12 -0400

    Added test_eq

--- medacy/tests/tools/test_entity.py
+++ medacy/tests/tools/test_entity.py
@@ -9,3 +9,9 @@ class TestEntity(unittest.TestCase):
         match = re.search("an example", "this is an example")
         actual = Entity.init_from_re_match(match, "Something")
         self.assertEqual(expected, actual)
+
+    def test_eq(self):
+        first = Entity("Something", 8, 18, "an example", 1)
+        second = Entity("Else", 8, 18, "an example", 5)
+        self.assertEqual(first, second)
+
