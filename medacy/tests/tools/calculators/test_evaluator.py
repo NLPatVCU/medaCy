6aa317eaf5d0c9e0aecb2930677e630aa5bb8398
swfarnsworth <swfarnsworth@gmail.com>
2019-07-25 17:50:55 -0400

    Changed the file structure

--- medacy/tests/tools/calculators/test_evaluator.py
+++ medacy/tests/tools/calculators/test_evaluator.py
@@ -8,15 +8,16 @@ class TestEvaluator(unittest.TestCase):
     def setUpClass(cls) -> None:
         """Store the locations of the sample data as attributes."""
         location = os.path.abspath(os.path.dirname(__file__))
-        cls.gold_data_dir_name = os.path.join(location, "sample_data/gold_ann_samples")
-        cls.false_data_dir_name = os.path.join(location, "sample_data/false_ann_samples")
+        cls.gold_data_dir_name = os.path.join(location, "../sample_data/gold_ann_samples")
+        cls.false_data_dir_name = os.path.join(location, "../sample_data/false_ann_samples")
 
     def test_evaluator(self):
         actual = evaluate_annotation_agreement(
             self.gold_data_dir_name,
-            self.gold_data_dir_name,
+            self.false_data_dir_name,
             entities=["adversereaction", "tradename"],
             relations=["Drug-ADE"],
             verbose=False
         )
         print(actual)
+
