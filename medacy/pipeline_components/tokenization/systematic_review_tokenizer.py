bf64ddeece25fbc9c39ae6a236051b907d920cf6
swfarnsworth <swfarnsworth@gmail.com>
2019-07-26 12:31:49 -0400

    minor refactoring

--- medacy/pipeline_components/tokenization/systematic_review_tokenizer.py
+++ medacy/pipeline_components/tokenization/systematic_review_tokenizer.py
@@ -3,7 +3,7 @@ from spacy.language import Language
 from spacy.tokenizer import Tokenizer
 from spacy.util import compile_infix_regex, compile_prefix_regex, compile_suffix_regex
 
-class SystematicReviewTokenizer():
+class SystematicReviewTokenizer:
     """
     A tokenizer for clinical text
     """
@@ -12,12 +12,14 @@ class SystematicReviewTokenizer():
         if not isinstance(nlp, Language):
             raise ValueError("NLP must be an instance of spacy.lang")
         self.nlp = nlp
-        self.tokenizer = Tokenizer(nlp.vocab, nlp.Defaults.tokenizer_exceptions,
-                                prefix_search=self._get_prefix_regex().search,
-                                infix_finditer=self._get_infix_regex().finditer,
-                                suffix_search=self._get_suffix_regex().search,
-                                token_match=None
-                     )
+        self.tokenizer = Tokenizer(
+            nlp.vocab,
+            nlp.Defaults.tokenizer_exceptions,
+            prefix_search=self._get_prefix_regex().search,
+            infix_finditer=self._get_infix_regex().finditer,
+            suffix_search=self._get_suffix_regex().search,
+            token_match=None
+        )
 
     def add_exceptions(self, exceptions):
         """
@@ -25,7 +27,7 @@ class SystematicReviewTokenizer():
         :param exceptions: an array of terms to not split on during tokenization
         :return:
         """
-        raise NotImplementedError()
+        raise NotImplementedError
 
     def _get_prefix_regex(self):
         """
