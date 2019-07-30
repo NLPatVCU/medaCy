3c34a534914cf33541cfc335cef36ab833cfe146
swfarnsworth <swfarnsworth@gmail.com>
2019-07-30 15:49:27 -0400

    Added a pipeline component for word embeddings

--- medacy/pipeline_components/embeddings/embedding_component.py
+++ medacy/pipeline_components/embeddings/embedding_component.py
@@ -1,14 +1,27 @@
+import importlib
 from spacy.tokens import Doc, Token
-from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
 from medacy.pipeline_components.base import BaseComponent
 
-class EmbeddingComponent(BaseComponent):
 
-    def __init__(self, spacy_pipeline):
+class EmbeddingComponent(BaseComponent):
+    """A pipeline component that gathers word embeddings for use as features using gensim."""
+
+    dependencies = []
+    name = "embedding_component"
+
+    def __init__(self, spacy_pipeline, word_embeddings):
         super().__init__(component_name="embedding_component")
         self.spacy_pipeline = spacy_pipeline
-        self.model = Word2Vec.load("some/path")
+        self.model = KeyedVectors.load_word2vec_format(word_embeddings, binary=True)
+
+    def _lookup_embedding(self, token):
+        try:
+            return list(self.model[token.text])
+        except KeyError:
+            return []
 
     def __call__(self, doc: Doc):
-        # self.model.similarity('france', 'spain')
-        pass
+        for token in doc:
+            Token.set_extension("feature_embedding", getter=self._lookup_embedding)
+        return doc
