292a13b4dfad066826ca888846088288b0760a4d
swfarnsworth <swfarnsworth@gmail.com>
2019-07-31 14:53:09 -0400

    Added gensim

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ import sys
 
 packages = find_packages()
 
+
 def readme():
     with open('README.md') as f:
         return f.read()
+
 
 class PyTest(TestCommand):
     """
@@ -27,6 +29,7 @@ class PyTest(TestCommand):
 
         errno = pytest.main(shlex.split(self.pytest_args))
         sys.exit(errno)
+
 
 setup(
     name='medacy',
@@ -63,6 +66,7 @@ setup(
         'pathos>=0.2.2.1',
         'msgpack>=0.3.0,<0.6',
         'msgpack-numpy<0.4.4.0',
+        'gensim>=3.8.0',
         'en_core_web_sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.1.0/en_core_web_sm-2.1.0.tar.gz#egg=en_core_web_sm-2.1.0'
     ],
     tests_require=["pytest", "pytest-cov", "medacy_dataset_end==1.0.3"],
