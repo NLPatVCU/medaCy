140f7b7713f3a8f8b44600ef5da9ca583097fda6
conteam <conteam@mymail.vcu.edu>
2019-09-27 18:55:50 -0400

    Remove extra scratch files

--- BiLSTM-CRF Development/loading_en_model.py
+++
@@ -1,4 +0,0 @@
-import spacy
-
-nlp = spacy.load('en_core_web_lg')
-
--- BiLSTM-CRF Development/n2c2_counting_entities.py
+++
@@ -1,9 +0,0 @@
-from pprint import pprint
-from medacy.data import Dataset
-
-n2c2_dataset = Dataset("/Users/annaconte/NLPatVCU/N2C2_Data")
-
-n2c2_dataset_counts = n2c2_dataset.compute_counts()
-
-pprint(n2c2_dataset_counts)
-
