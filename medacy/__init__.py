90ec31295375c8c86b870566f345660541d09276
swfarnsworth <swfarnsworth@gmail.com>
2019-10-01 15:40:08 -0400

    adding commonly-used classes to __init__ for easy access in external scripts

--- medacy/__init__.py
+++ medacy/__init__.py
@@ -1,2 +1,9 @@
 __version__ = '0.1.1'
 __authors__ = "Andriy Mulyar, Jorge Vargas, Corey Sutphin, Bobby Best, Steele Farnsworth, Bridget T. McInnes"
+
+
+# These classes are part of the interface, therefore they're available to import directly from the top-level
+# Do not import `from medacy import Model` internally.
+from medacy.data.dataset import Dataset
+from medacy.model.model import Model
+from medacy.pipeline_components.feature_overlayers.metamap.metamap import MetaMap
