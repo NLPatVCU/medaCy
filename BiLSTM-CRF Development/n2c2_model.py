6d0be6753957ed2ee2f56845ed372ba34f258f03
conteam <conteam@mymail.vcu.edu>
2019-10-03 19:40:37 -0400

    Remove file limit

--- BiLSTM-CRF Development/n2c2_model.py
+++ BiLSTM-CRF Development/n2c2_model.py
@@ -4,7 +4,7 @@ from medacy.ner.pipelines import LstmSystematicReviewPipeline
 import logging, sys
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-n2c2_dataset = Dataset(data_directory="/home/share/N2C2", data_limit=1)
+n2c2_dataset = Dataset(data_directory="/home/share/N2C2")
 
 pipeline = LstmSystematicReviewPipeline(entities=['ADE', 'Dosage', 'Drug'],
                                         word_embeddings='/home/conteam/mimic3_d200.bin')
@@ -21,10 +21,3 @@ model.cross_validate(training_dataset=n2c2_dataset, num_folds=2)
 
 
 #word_embeddings='/Users/annaconte/NLPatVCU/Datasets/cd ..
-
-
-
-
-
-
-
