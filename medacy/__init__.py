e1cb2c2b02cbbcde6b25d01f32e71bafbd5d968d
swfarnsworth <swfarnsworth@gmail.com>
2019-10-14 23:36:59 -0400

    restructuring is not backwards compatible, ergo increment version number

--- medacy/__init__.py
+++ medacy/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '0.1.1'
-__authors__ = "Andriy Mulyar, Jorge Vargas, Corey Sutphin, Bobby Best, Steele Farnsworth, Bridget T. McInnes"
+__version__ = '1.0.0'
+__authors__ = "Andriy Mulyar, Jorge Vargas, Corey Sutphin, Steele Farnsworth, Bobby Best, Bridget T. McInnes"
