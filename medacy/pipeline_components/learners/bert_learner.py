d741f88d9d58ec92408ae9077db59c9182a44b96
Jorge Vargas <jvargas2@icloud.com>
2019-12-16 16:30:00 -0500

    Start commenting and fix extra special tokens issue

--- medacy/pipeline_components/learners/bert_learner.py
+++ medacy/pipeline_components/learners/bert_learner.py
@@ -1,5 +1,3 @@
-"""Learner for running predictions and fine tuning BERT models.
-"""
 import logging
 import os
 import torch
@@ -8,74 +6,130 @@ from transformers import AdamW, BertTokenizer, BertForTokenClassification
 from medacy.nn import Vectorizer
 
 class BertLearner:
+    """Learner for running predictions and fine tuning BERT models.
+    """
+
     def __init__(self, cuda_device=-1, pretrained_model='bert-large-cased'):
-        torch.manual_seed(1)
+        """
+        Initialize BertLearner.
+
+        :param cuda_device: CUDA device to use when running on a GPU. Use -1 for CPU.
+        :param pretrained_model: Name of model to use for the Transformers pretrained_model. This
+            is different from the medaCy fine-tuned model you may have created.
+        """
+        torch.manual_seed(1) # Seed PyTorch for consistency
+
+        # Create torch.device that all tensors will be using
         device_string = 'cuda:%d' % cuda_device if cuda_device >= 0 else 'cpu'
         self.device = torch.device(device_string)
 
-        self.model = None
-        self.tokenizer = None
-        self.vectorizer = Vectorizer(self.device)
-        self.pretrained_model = pretrained_model
+        self.model = None # Transformers/PyTorch model
+        self.tokenizer = None # Transformers tokenizer
+        self.vectorizer = Vectorizer(self.device) # medaCy vectorizer
+        self.pretrained_model = pretrained_model # Name of Transformers pretrained model
 
     def encode_sequences(self, sequences, labels=[]):
+        """
+        Encode a list of text sequences into encoded sequences for the Transformers package
+
+        :param sequences: List of sequences. Each sequence is a list of strings that represent
+            tokens from the original sentence.
+        :param labels: List of list of labels. There's one label for each token in sequences.
+            Labels are represented by strings.
+        """
         encoded_sequences = []
 
+        # If there are no labels then make all of the labels 'O'
         if not labels:
             for sequence in sequences:
                 labels.append(['O'] * len(sequence))
 
+        # Use the tokenizer to encode every sequence and add in special tokens like [CLS] and [SEP]
+        # Then add them to encoded_sequences
         for sequence in sequences:
             encoded_sequence = []
             for token in sequence:
-                encoded_sequence.append(self.tokenizer.encode(token))
+                encoded_sequence.append(self.tokenizer.encode(token, add_special_tokens=False))
             encoded_sequence = self.tokenizer.build_inputs_with_special_tokens(encoded_sequence)
             encoded_sequences.append(encoded_sequence)
 
+        # Encoding them this way makes it possible to track which tokens were split up. Now we can
+        # store them correctly in these variables along with a mapping of the original tokens.
         split_sequences = []
         split_sequence_labels = []
         mappings = []
 
+        # Iterate through encoded sequences and original labels. Each token in the sequnece is a
+        # list that will be length 1 if the token wasn't split, but longer if it was.
         for sequence, sequence_labels in zip(encoded_sequences, labels):
+            # First token is always encoding of [CLS]. Create new lists starting with it and the
+            # proper label/mapping
             split_sequence = [sequence[0]]
             split_labels = ['O']
             mapping = [0]
 
+            # Loop through ids and labels in sequences. Don't take first or last id lists from
+            # sequence since we already added [CLS] and will add [SEP] later.
             for ids, label in zip(sequence[1:-1], sequence_labels):
-                map_value = 1
+                map_value = 1 # First token id is the only one we want to classify
+
                 for token_id in ids:
+                    # Add proper token id and label to lists
                     split_sequence.append(token_id)
                     split_labels.append(label)
+
+                    # Add map value of 1 the first time but 0 every other time
                     mapping.append(map_value)
                     map_value = 0
 
+            # Add the final token [SEP] with proper label/mapping
             split_sequence.append(sequence[-1])
             split_labels.append('O')
             mapping.append(0)
 
+            # Append final forms of lists
             split_sequences.append(split_sequence)
             split_sequence_labels.append(self.vectorizer.vectorize_tags(split_labels))
             mappings.append(mapping)
 
+        # Return the there lists that were created
         return split_sequences, split_sequence_labels, mappings
 
     def decode_labels(self, sequence_labels, mappings):
+        """
+        Decode list of label indices using mappings generated during self.encode_sequences()
+
+        :param sequence_labels: List of list of label indices. Index corresponds to index of label
+            in self.vectorizer
+        :param mappings: List of list of mappings. All mappings indicate whether we should include
+            it in classification (1) or not (0)
+        """
         decoded_labels = []
 
         for labels, mapping in zip(sequence_labels, mappings):
             remapped_labels = []
 
             for label, map_value in zip(labels, mapping):
+                # Only include label if map value is 1
                 if map_value == 1:
                     remapped_labels.append(label)
 
+            # Decode list of label indices useing self.vectorizer
             decoded_labels.append(self.vectorizer.devectorize_tag(remapped_labels))
 
         return decoded_labels
 
     def fit(self, x_data, y_data):
+        """Finetune a pretrained BERT model using the Transformers package.
+
+        :param x_data: List of sequences. Each sequence is a list of strings that represent
+            tokens from the original sentence.
+        :param y_data: List of list of labels. There's one label for each token in sequences.
+            Labels are represented by strings.
+        """
         self.vectorizer.create_tag_dictionary(y_data)
 
+        # Load pretrain BERT model, unfreeze layers, move it to GPU device, and create its tokenizer
         self.model = BertForTokenClassification.from_pretrained(
             self.pretrained_model,
             num_labels=len(self.vectorizer.tag_to_index)
@@ -84,6 +138,7 @@ class BertLearner:
         self.model.to(device=self.device)
         self.tokenizer = BertTokenizer.from_pretrained(self.pretrained_model)
 
+        # Use Adam optimizer with weight decay options as shown in the link below:
         # https://github.com/huggingface/transformers/blob/master/examples/run_ner.py
         no_decay = ["bias", "LayerNorm.weight"]
         optimizer_grouped_parameters = [
@@ -93,17 +148,22 @@ class BertLearner:
         ]
         optimizer = AdamW(optimizer_grouped_parameters, lr=5e-5, eps=1e-8)
 
+        # Encode sequencesa and labels
         x_data, y_data, _ = self.encode_sequences(x_data, y_data)
         
+        # Only do 3 epochs as suggested in BERT paper
         for epoch in range(3):
             logging.info('Epoch %d' % epoch)
 
             for sequence, labels in zip(x_data, y_data):
+                # Unsqueeze sequences and labels since we're doing a batch size of 1
                 sequence = torch.tensor(sequence, device=self.device).unsqueeze(0)
                 labels = labels.unsqueeze(0)
 
+                # Pass sequences through the model and get the loss
                 loss, _ = self.model(sequence, labels=labels)
 
+                # Train using the optimizer and loss
                 loss.backward()
                 optimizer.step()
                 optimizer.zero_grad()
