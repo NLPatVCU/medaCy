70e0a158592b10957396be15050e8fed136bb65a
swfarnsworth <swfarnsworth@gmail.com>
2019-12-19 13:05:21 -0500

    added tests for predict; if this test causes a new directory to be created, the manifest will ignore it.

--- medacy/tests/model/test_model.py
+++ medacy/tests/model/test_model.py
@@ -20,11 +20,13 @@ class TestModel(TestCase):
         cls.dataset = Dataset(os.path.join(test_dir, 'sample_dataset_1'))
         cls.entities = cls.dataset.get_labels(as_list=True)
         cls.prediction_directory = tempfile.mkdtemp()  # directory to store predictions
+        cls.prediction_directory_2 = tempfile.mkdtemp()
 
     @classmethod
     def tearDownClass(cls):
         pkg_resources.cleanup_resources()
         shutil.rmtree(cls.prediction_directory)
+        shutil.rmtree(cls.prediction_directory_2)
 
     def test_fit_predict_dump_load(self):
         """Fits a model, tests that it predicts correctly, dumps and loads it, then tests that it still predicts"""
@@ -58,3 +60,41 @@ class TestModel(TestCase):
 
         resulting_ann = new_model.predict('To exclude the possibility that alterations in PSSD might be a consequence of changes in the volume of reference, we used a subset of the vibratome sections')
         self.assertIsInstance(resulting_ann, Annotations)
+
+    def test_predict(self):
+        """
+        predict() has different functionality depending on what is passed to it; therefore this test
+        ensures that each type of input is handled correctly
+        """
+
+        # Init the Model
+        pipe = TestingPipeline(entities=self.entities)
+        sample_model_path = os.path.join(test_dir, 'sample_models', 'sample_test_pipe.pkl')
+        model = Model(pipe)
+        model.load(sample_model_path)
+
+        # Test passing a Dataset
+        dataset_output = model.predict(self.dataset)
+        self.assertIsInstance(dataset_output, Dataset)
+        self.assertEqual(len(dataset_output), len(self.dataset))
+
+        # Test passing a directory
+        directory_output = model.predict(self.dataset.data_directory)
+        self.assertIsInstance(directory_output, Dataset)
+        self.assertEqual(len(directory_output), len(self.dataset))
+
+        # Test passing a string
+        string_output = model.predict('This is a sample string.')
+        self.assertIsInstance(string_output, Annotations)
+
+        # Test that the predictions are written to the expected location when no path is provided
+        expected_dir = os.path.join(self.dataset.data_directory, 'predictions')
+        self.assertTrue(os.path.isdir(expected_dir))
+
+        # Delete that directory
+        shutil.rmtree(expected_dir)
+
+        # Test predicting to a specific directory
+        model.predict(self.dataset.data_directory, prediction_directory=self.prediction_directory_2)
+        expected_files = os.listdir(self.prediction_directory_2)
+        self.assertEqual(6, len(expected_files))
