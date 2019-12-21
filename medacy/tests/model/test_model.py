2144e54b26447fb1bfc3a682bc5efcd3fa3f9f0a
swfarnsworth <swfarnsworth@gmail.com>
2019-12-21 18:05:21 -0500

    added tests for cross_validate, _run_through_pipeline

--- medacy/tests/model/test_model.py
+++ medacy/tests/model/test_model.py
@@ -21,6 +21,7 @@ class TestModel(TestCase):
         cls.entities = cls.dataset.get_labels(as_list=True)
         cls.prediction_directory = tempfile.mkdtemp()  # directory to store predictions
         cls.prediction_directory_2 = tempfile.mkdtemp()
+        cls.pipeline = TestingPipeline(entities=cls.entities)
 
     @classmethod
     def tearDownClass(cls):
@@ -31,8 +32,7 @@ class TestModel(TestCase):
     def test_fit_predict_dump_load(self):
         """Fits a model, tests that it predicts correctly, dumps and loads it, then tests that it still predicts"""
 
-        pipeline = TestingPipeline(entities=self.entities)
-        model = Model(pipeline)
+        model = Model(self.pipeline)
 
         # Test attempting to predict before fitting
         with self.assertRaises(RuntimeError):
@@ -55,7 +55,7 @@ class TestModel(TestCase):
         # Test pickling a model
         pickle_path = os.path.join(self.prediction_directory, 'test.pkl')
         model.dump(pickle_path)
-        new_model = Model(pipeline)
+        new_model = Model(self.pipeline)
         new_model.load(pickle_path)
 
         resulting_ann = new_model.predict('To exclude the possibility that alterations in PSSD might be a consequence of changes in the volume of reference, we used a subset of the vibratome sections')
@@ -98,3 +98,29 @@ class TestModel(TestCase):
         model.predict(self.dataset.data_directory, prediction_directory=self.prediction_directory_2)
         expected_files = os.listdir(self.prediction_directory_2)
         self.assertEqual(6, len(expected_files))
+
+    def test_cross_validate(self):
+        """Ensures that changes made in the package do not prevent cross_validate from running to completion"""
+        model = Model(self.pipeline)
+        try:
+            model.cross_validate(self.dataset, 2)
+        except:
+            self.assertTrue(False)
+
+    def test_run_through_pipeline(self):
+        """
+        Tests that this function runs a document through the pipeline by testing that it has attributes
+        overlayed by the pipeline
+        """
+        model = Model(self.pipeline)
+        sample_df = list(self.dataset)[0]
+        result = model._run_through_pipeline(sample_df)
+
+        expected = sample_df.txt_path
+        actual = result._.file_name
+        self.assertEqual(actual, expected)
+
+        expected = sample_df.ann_path
+        actual = result._.gold_annotation_file
+        self.assertEqual(actual, expected)
+
