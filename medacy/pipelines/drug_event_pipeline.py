982453c9feea8d395f76983948b6e0684e07898e
swfarnsworth <swfarnsworth@gmail.com>
2020-01-14 08:51:12 -0500

    Created function to return CRF instance; changed all CRF-using pipelines to call this function; drug_event_pipeline uses MetaMapAllTypesComponent; using "if metamap" instead of "if isinstance(metamap, MetaMap)" so that users get an error if they try to add MetaMap and it is not correct

--- medacy/pipelines/drug_event_pipeline.py
+++ medacy/pipelines/drug_event_pipeline.py
@@ -1,10 +1,10 @@
-import sklearn_crfsuite
 import spacy
 
 from medacy.pipeline_components.feature_extractors.discrete_feature_extractor import FeatureExtractor
 from medacy.pipeline_components.feature_overlayers.lexicon_component import LexiconOverlayer
-from medacy.pipeline_components.feature_overlayers.metamap.metamap_component import MetaMapOverlayer
+from medacy.pipeline_components.feature_overlayers.metamap.metamap_all_types_component import MetaMapAllTypesOverlayer
 from medacy.pipeline_components.feature_overlayers.table_matcher_component import TableMatcherOverlayer
+from medacy.pipeline_components.learners.crf_learner import get_crf
 from medacy.pipeline_components.tokenizers.character_tokenizer import CharacterTokenizer
 from medacy.pipelines.base.base_pipeline import BasePipeline
 
@@ -26,155 +26,19 @@ class DrugEventPipeline(BasePipeline):
         """
         super().__init__(entities, spacy_pipeline=spacy.load("en_core_web_sm"))
 
-        if metamap is not None:
-            self.add_component(MetaMapOverlayer, metamap, semantic_type_labels=[
-                                                                        'aapp',
-                                                                        'acab',
-                                                                        'acty',
-                                                                        'aggp',
-                                                                        'amas',
-                                                                        'amph',
-                                                                        'anab',
-                                                                        'anim',
-                                                                        'anst',
-                                                                        'antb',
-                                                                        'arch',
-                                                                        'bacs',
-                                                                        'bact',
-                                                                        'bdsu',
-                                                                        'bdsy',
-                                                                        'bhvr',
-                                                                        'biof',
-                                                                        'bird',
-                                                                        'blor',
-                                                                        'bmod',
-                                                                        'bodm',
-                                                                        'bpoc',
-                                                                        'bsoj',
-                                                                        'celc',
-                                                                        'celf',
-                                                                        'cell',
-                                                                        'cgab',
-                                                                        'chem',
-                                                                        'chvf',
-                                                                        'chvs',
-                                                                        'clas',
-                                                                        'clna',
-                                                                        'clnd',
-                                                                        'cnce',
-                                                                        'comd',
-                                                                        'crbs',
-                                                                        'diap',
-                                                                        'dora',
-                                                                        'drdd',
-                                                                        'dsyn',
-                                                                        'edac',
-                                                                        'eehu',
-                                                                        'elii',
-                                                                        'emod',
-                                                                        'emst',
-                                                                        'enty',
-                                                                        'enzy',
-                                                                        'euka',
-                                                                        'evnt',
-                                                                        'famg',
-                                                                        'ffas',
-                                                                        'fish',
-                                                                        'fndg',
-                                                                        'fngs',
-                                                                        'food',
-                                                                        'ftcn',
-                                                                        'genf',
-                                                                        'geoa',
-                                                                        'gngm',
-                                                                        'gora',
-                                                                        'grpa',
-                                                                        'grup',
-                                                                        'hcpp',
-                                                                        'hcro',
-                                                                        'hlca',
-                                                                        'hops',
-                                                                        'horm',
-                                                                        'humn',
-                                                                        'idcn',
-                                                                        'imft',
-                                                                        'inbe',
-                                                                        'inch',
-                                                                        'inpo',
-                                                                        'inpr',
-                                                                        'irda',
-                                                                        'lang',
-                                                                        'lbpr',
-                                                                        'lbtr',
-                                                                        'mamm',
-                                                                        'mbrt',
-                                                                        'mcha',
-                                                                        'medd',
-                                                                        'menp',
-                                                                        'mnob',
-                                                                        'mobd',
-                                                                        'moft',
-                                                                        'mosq',
-                                                                        'neop',
-                                                                        'nnon',
-                                                                        'npop',
-                                                                        'nusq',
-                                                                        'ocac',
-                                                                        'ocdi',
-                                                                        'orch',
-                                                                        'orga',
-                                                                        'orgf',
-                                                                        'orgm',
-                                                                        'orgt',
-                                                                        'ortf',
-                                                                        'patf',
-                                                                        'phob',
-                                                                        'phpr',
-                                                                        'phsf',
-                                                                        'phsu',
-                                                                        'plnt',
-                                                                        'podg',
-                                                                        'popg',
-                                                                        'prog',
-                                                                        'pros',
-                                                                        'qlco',
-                                                                        'qnco',
-                                                                        'rcpt',
-                                                                        'rept',
-                                                                        'resa',
-                                                                        'resd',
-                                                                        'rnlw',
-                                                                        'sbst',
-                                                                        'shro',
-                                                                        'socb',
-                                                                        'sosy',
-                                                                        'spco',
-                                                                        'tisu',
-                                                                        'tmco',
-                                                                        'topp',
-                                                                        'virs',
-                                                                        'vita',
-                                                                        'vtbt'
-                                                                        ]
-                               )
+        if metamap:
+            self.add_component(MetaMapAllTypesOverlayer, metamap)
+
         if lexicon is not None:
             self.add_component(LexiconOverlayer, lexicon)
 
         self.add_component(TableMatcherOverlayer)
 
     def get_learner(self):
-        return ("CRF_l2sgd",
-                sklearn_crfsuite.CRF(
-                    algorithm='l2sgd',
-                    c2=0.1,
-                    max_iterations=100,
-                    all_possible_transitions=True
-                )
-            )
+        return "CRF_l2sgd", get_crf()
 
     def get_tokenizer(self):
         return CharacterTokenizer(self.spacy_pipeline)
 
     def get_feature_extractor(self):
-        extractor = FeatureExtractor(window_size=3, spacy_features=['pos_', 'shape_', 'prefix_', 'suffix_', 'like_num', 'text', 'head'])
-        return extractor
+        return FeatureExtractor(window_size=3, spacy_features=['pos_', 'shape_', 'prefix_', 'suffix_', 'like_num', 'text', 'head'])
--- medacy/pipelines/systematic_review_pipeline.py
+++ medacy/pipelines/systematic_review_pipeline.py
@@ -1,9 +1,9 @@
-import sklearn_crfsuite
 import spacy
 
 from medacy.pipeline_components.feature_extractors.discrete_feature_extractor import FeatureExtractor
 from medacy.pipeline_components.feature_overlayers.metamap.metamap import MetaMap
 from medacy.pipeline_components.feature_overlayers.metamap.metamap_component import MetaMapOverlayer
+from medacy.pipeline_components.learners.crf_learner import get_crf
 from medacy.pipeline_components.tokenizers.systematic_review_tokenizer import SystematicReviewTokenizer
 from medacy.pipelines.base.base_pipeline import BasePipeline
 
@@ -33,14 +33,7 @@ class SystematicReviewPipeline(BasePipeline):
             self.add_component(MetaMapOverlayer, metamap)
 
     def get_learner(self):
-        return ("CRF_l2sgd",
-                sklearn_crfsuite.CRF(
-                    algorithm='l2sgd',
-                    c2=0.1,
-                    max_iterations=100,
-                    all_possible_transitions=True
-                    )
-                )
+        return "CRF_l2sgd", get_crf()
 
     def get_tokenizer(self):
         return SystematicReviewTokenizer(self.spacy_pipeline)
