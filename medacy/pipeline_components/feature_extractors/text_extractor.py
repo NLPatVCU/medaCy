f925b34956d89ca1cc7c940bb5654ba3076418ee
ggurdin <ggurdin@gmail.com>
2020-08-03 18:33:57 -0400

    Added span splitting code

--- medacy/pipeline_components/feature_extractors/text_extractor.py
+++ medacy/pipeline_components/feature_extractors/text_extractor.py
@@ -1,5 +1,6 @@
 from medacy.pipeline_components.feature_extractors import FeatureTuple
 from medacy.pipeline_components.feature_extractors.discrete_feature_extractor import FeatureExtractor
+from transformers import AdamW, BertTokenizer, BertForTokenClassification
 
 
 class TextExtractor(FeatureExtractor):
@@ -38,6 +39,45 @@ class TextExtractor(FeatureExtractor):
         :return: Tuple of parallel lists, a list of token texts and a list of corresponding character spans
         """
 
+        """
+        features = []
+        for sent in doc.sents:
+            features = [token.text for token in sent]
+        """
+
+        tokenizer = BertTokenizer.from_pretrained('bert-large-cased')
+
+        features, indices = [], []
+        for sent in doc.sents:
+            feature = [token.text for token in sent]
+            index = [(token.idx, token.idx + len(token)) for token in sent]
+            encoded_sequence = [tokenizer.encode(token, add_special_tokens=False) for token in feature]
+            encoded_sequence = tokenizer.build_inputs_with_special_tokens(encoded_sequence)
+            bert_tokenized = []
+            for x in encoded_sequence:
+                if type(x) == int:
+                    bert_tokenized.append(x)
+                else:
+                    bert_tokenized.extend(x)
+            if len(bert_tokenized) > 500:
+                num_splits = (len(bert_tokenized) // 500) + 3
+                split_length = int(len(feature) / num_splits) + 1
+                feature_splits = list(chunks(feature, split_length))
+                indices_splits = list(chunks(index, split_length))
+                features.extend(feature_splits)
+                indices.extend(indices_splits)
+            else:
+                features.append(feature)
+                indices.append(index)
+
+        """        
         features = [[token.text for token in sent] for sent in doc.sents]
         indices = [[(token.idx, token.idx + len(token)) for token in sent] for sent in doc.sents]
+        """
         return features, indices
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
